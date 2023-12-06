85b1da4ae449e962aa0f1492165443bc205381c2
Hodong Kim <hodong@nimfsoft.com>
2023-12-06 18:34:01 +0900

    implement nonblocking FastCGI I/O

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 CFLAGS = -Wall -I/usr/local/include -I/home/hodong/projects/nimf/clair/src
 LIBADD = /home/hodong/projects/nimf/clair/src/libclair.a
+TARGET = checkout
 
-C_SOURCES = checkout.c
-C_HEADERS =
+C_SOURCES = $(TARGET).c
+C_HEADERS = fastcgi.h
 
 checkout: Makefile $(C_SOURCES) $(C_HEADERS)
-	$(CC) $(CFLAGS) $(C_SOURCES) $(LIBADD) -o checkout
+	$(CC) $(CFLAGS) $(C_SOURCES) $(LIBADD) -o $(TARGET)
+
+clean:
+	rm -f $(TARGET) $(TARGET).sock
--- checkout.c
+++ checkout.c
@@ -17,52 +17,47 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
-
-#define FCGI_HEADER_LEN  8
-#define LEN 4098
-
-typedef struct _FcgiHeader FcgiHeader;
-struct __attribute__((packed)) _FcgiHeader {
-    uint8_t version;
-    uint8_t type;
-    uint8_t req_id_b1;
-    uint8_t req_id_b0;
-    uint8_t content_len_b1;
-    uint8_t content_len_b0;
-    uint8_t padding_len;
-    uint8_t reserved;
-};
+#include "fastcgi.h"
 
 enum _State : uint8_t {
-  RECV_HEADER,
-  RECV_CONTENT
+  RECV_HEADER =  1 << 0,
+  RECV_CONTENT = 1 << 1,
+  SEND
 };
 typedef enum _State State;
 
 typedef struct _Conn Conn;
 struct _Conn {
-  FcgiHeader header;
   uint8_t* buf;
   size_t   pos;
   size_t   buf_capa;
-  size_t   res_len;
+  size_t   packet_len;
+  // header
+  uint8_t  version;
+  uint8_t  type;
+  uint16_t req_id;
+  uint16_t content_len;
+  uint16_t padding_len;
+  // begin req body
+  uint16_t role;
+  uint8_t  flags;
+
   State    state; /* uint8_t */
 };
 
 Conn* conn_new ()
 {
   #define CONN_BUF_DEFAULT_CAPA 16
-  Conn* conn = c_malloc (sizeof (Conn));
+  Conn* conn = c_calloc (1, sizeof (Conn));
   conn->buf_capa = CONN_BUF_DEFAULT_CAPA;
-  conn->buf      = c_malloc (CONN_BUF_DEFAULT_CAPA);
-  conn->pos      = 0;
-  conn->res_len  = 0;
-  conn->state    = RECV_HEADER;
+  conn->buf   = c_malloc (CONN_BUF_DEFAULT_CAPA);
+  conn->state = RECV_HEADER;
   return conn;
 }
 
 void conn_free (Conn* conn)
 {
+  c_log_info ("conn_free");
   free (conn->buf);
   free (conn);
 }
@@ -82,98 +77,56 @@ void conn_resize_capa (Conn* conn, size_t req_len)
 }
 
 CLoop* loop;
-#if 0
-static void cb_write (int sockfd, short revents, void* user_data)
-{
-  printf ("%ld >> cb_write\n", time (NULL));
-  FCGX_Request* req = user_data;
-  FCGX_FPrintF (req->out,
-         "Status: 200 OK\r\n"
-         "Content-type: text/html\r\n"
-         "\r\n"
-         "<h1>Example: FastCGI Multiplex</h1>");
-  FCGX_Finish_r (req);
-  free (req);
-  printf ("%ld << cb_write\n", time (NULL));
-  c_loop_remove_fd (loop, sockfd);
-}
-#endif
 
-static void cb_read_header (int sockfd, short revents, void* user_data);
+static void cb_incoming (int sockfd, short revents, void* user_data);
 
-static void cb_read_content (int sockfd, short revents, void* user_data)
+static void cb_send (int sockfd, short revents, void* user_data)
 {
-  c_log_info ("cb_read_content from %d", sockfd);
+  c_log_info ("%ld cb_send", time (NULL));
   Conn* conn = user_data;
 
-  if (revents & (POLLHUP | POLLERR))
+  if (conn->state != SEND)
   {
-    c_log_critical ("POLLHUP | POLLERR");
-    c_loop_remove_fd (loop, sockfd);
-    conn_free (conn);
-    close (sockfd);
-    return;
+    c_log_warning ("Programming error");
+    abort ();
   }
 
   ssize_t n_bytes;
-  int content_len;
-  void* buf;
-  FcgiHeader* header = &conn->header;
+  n_bytes = send (sockfd, conn->buf + conn->pos,
+                  conn->packet_len - conn->pos, 0);
 
-  content_len = (header->content_len_b1 << 8) + header->content_len_b0;
-
-  c_log_info ("content_len: %d", content_len);
-  c_log_info ("conn->pos: %lu", conn->pos);
-  conn_resize_capa (conn, content_len);
-  buf = conn->buf;
-
-  n_bytes = recv (sockfd, buf + conn->pos, content_len - conn->pos, 0);
-
-  // 클라이언트가 보낸 데이터를 어떠한 이유로 받을 수 없는 경우가 있습니다.
-  // timer 를 설정하여 클라이언트와의 접속을 끊는 기능을 추가해야 하지만
-  // 시간 관계상 생략합니다.
-
-  if (n_bytes > 0)
+  if (n_bytes >= 0)
   {
     conn->pos += n_bytes;
 
-    if (conn->pos == content_len)
+    if (conn->pos == conn->packet_len)
     {
-      c_log_info ("Receiving the content has been completed.");
-      conn->pos = 0;
-      // c_loop_mod_fd (loop, sockfd, POLLIN, cb_read_content, conn);
       c_loop_remove_fd (loop, sockfd);
-      c_loop_add_fd (loop, sockfd, POLLIN, cb_read_header, conn);
+
+      if ((conn->flags & FCGI_KEEP_CONN) == 0)
+      {
+        c_log_info ("(conn->flags & FCGI_KEEP_CONN) == 0");
+        conn_free (conn);
+        close (sockfd);
+        return;
+      }
+      else
+      {
+        c_loop_add_fd (loop, sockfd, POLLIN, cb_incoming, conn);
+        conn->state = RECV_HEADER;
+        conn->pos = 0;
+      }
     }
   }
-  else if (n_bytes == 0)
+  else if (n_bytes < 0)
   {
-    c_log_info ("ZERO BYTES");
-    /* linux man page
-       When a stream socket peer has performed an orderly shutdown, the
-       return value will be 0 (the traditional "end-of-file" return).
-
-       Datagram sockets in various domains (e.g., the UNIX and Internet
-       domains) permit zero-length datagrams.  When such a datagram is
-       received, the return value is 0.
-
-       The value 0 may also be returned if the requested number of bytes
-       to receive from a stream socket was 0.
-     */
-    c_loop_remove_fd (loop, sockfd);
-    conn_free (conn);
-    close (sockfd);
-  }
-  else
-  {
-    if (errno == EWOULDBLOCK ||
-        errno == EAGAIN)
+    if (errno == EWOULDBLOCK || errno == EAGAIN)
     {
       c_log_info ("%s", strerror (errno));
     }
     else
     {
-      c_log_critical ("%s", strerror (errno));
+      c_log_critical ("send() failed: %s", strerror (errno));
       c_loop_remove_fd (loop, sockfd);
       conn_free (conn);
       close (sockfd);
@@ -181,9 +134,9 @@ static void cb_read_content (int sockfd, short revents, void* user_data)
   }
 }
 
-static void cb_read_header (int sockfd, short revents, void* user_data)
+static void cb_incoming (int sockfd, short revents, void* user_data)
 {
-  c_log_info ("cb_read_header from %d", sockfd);
+  c_log_info ("cb_incoming from %d", sockfd);
   Conn* conn = user_data;
 
   if (revents & (POLLHUP | POLLERR))
@@ -197,12 +150,14 @@ static void cb_read_header (int sockfd, short revents, void* user_data)
 
   ssize_t n_bytes;
   int len;
-  void* buf;
 
-  len = FCGI_HEADER_LEN;
-  buf = &conn->header;
+  if (conn->state == RECV_HEADER)
+    len = FCGI_HEADER_LEN;
+  else
+    len = conn->content_len + conn->padding_len;
 
-  n_bytes = recv (sockfd, buf + conn->pos, len - conn->pos, 0);
+  conn_resize_capa (conn, len);
+  n_bytes = recv (sockfd, conn->buf + conn->pos, len - conn->pos, 0);
 
   // 클라이언트가 보낸 데이터를 어떠한 이유로 받을 수 없는 경우가 있습니다.
   // timer 를 설정하여 클라이언트와의 접속을 끊는 기능을 추가해야 하지만
@@ -212,26 +167,129 @@ static void cb_read_header (int sockfd, short revents, void* user_data)
   {
     conn->pos += n_bytes;
 
-    if (conn->pos == FCGI_HEADER_LEN)
+    // recv complete
+    if (conn->pos == len)
     {
-      c_log_info ("Receiving the header has been completed.");
-      FcgiHeader* header = &conn->header;
-      c_log_info ("\n"
-                  "version: %hhu\n"
-                  "type: %hhu\n"
-                  "req_id_b1: %hhu\n"
-                  "req_id_b0: %hhu\n"
-                  "content_len_b1: %hhu\n"
-                  "content_len_b0: %hhu\n"
-                  "padding_len: %hhu\n"
-                  "reserved: %hhu\n",header->version, header->type,
-                  header->req_id_b1, header->req_id_b0,
-                  header->content_len_b1, header->content_len_b0,
-                  header->padding_len, header->reserved);
-      conn->pos = 0;
-      // c_loop_mod_fd (loop, sockfd, POLLIN, cb_read_content, conn);
-      c_loop_remove_fd (loop, sockfd);
-      c_loop_add_fd (loop, sockfd, POLLIN, cb_read_content, conn);
+      if (conn->state == RECV_HEADER)
+      {
+        c_log_info ("----------");
+        c_log_info ("Receiving the header has been completed.");
+        FCGI_Header* header = (FCGI_Header*) conn->buf;
+        c_log_info ("version: %hhu", header->version);
+        c_log_info ("type: %hhu", header->type);
+        c_log_info ("requestIdB1: %hhu", header->requestIdB1);
+        c_log_info ("requestIdB0: %hhu", header->requestIdB0);
+        c_log_info ("contentLengthB1: %hhu", header->contentLengthB1);
+        c_log_info ("contentLengthB0: %hhu", header->contentLengthB0);
+        c_log_info ("paddingLength: %hhu", header->paddingLength);
+        c_log_info ("reserved: %hhu", header->reserved);
+        conn->version = header->version;
+        conn->type    = header->type;
+        conn->req_id  = (header->requestIdB1 << 8) + header->requestIdB0;
+        conn->content_len = (header->contentLengthB1 << 8) + header->contentLengthB0;
+        conn->padding_len = header->paddingLength;
+        conn->state = RECV_CONTENT;
+        conn->pos = 0;
+        c_log_info ("req_id: %hu", conn->req_id);
+        c_log_info ("content_len: %hu", conn->content_len);
+        if (conn->type != FCGI_BEGIN_REQUEST &&
+            conn->req_id == FCGI_NULL_REQUEST_ID) // 0
+        {
+          abort ();
+        }
+      }
+      else // RECV_CONTENT
+      {
+        c_log_info ("-------------------");
+        c_log_info ("Receiving the content has been completed.");
+        switch (conn->type)
+        {
+          case FCGI_BEGIN_REQUEST: // 1
+            {
+              FCGI_BeginRequestBody* body = (FCGI_BeginRequestBody*) conn->buf;
+              conn->role  = (body->roleB1 << 8) + body->roleB0;
+              c_log_info ("role:  %hu",  conn->role);
+              c_log_info ("flags: %hhu", conn->flags);
+              conn->flags = body->flags & FCGI_KEEP_CONN;
+              conn->state = RECV_HEADER;
+              conn->pos = 0;
+              // send end req rec
+              /*
+              conn_resize_capa (conn, sizeof (FCGI_EndRequestRecord));
+              FCGI_EndRequestRecord* rec = (FCGI_EndRequestRecord*) conn->buf;
+              rec->header.version = FCGI_VERSION_1;
+              rec->header.type = FCGI_END_REQUEST;
+
+              rec->header.requestIdB1 = (uint8_t) ((conn->req_id >> 8) & 0xff);
+              rec->header.requestIdB0 = (uint8_t) (conn->req_id & 0xff);
+
+              c_log_info ("rec->body len: %d", (int) sizeof (rec->body));
+
+              rec->header.contentLengthB1 = (uint8_t) ((sizeof (rec->body) >> 8) & 0xff);
+              rec->header.contentLengthB0 = (uint8_t) (sizeof (rec->body) & 0xff);
+
+              rec->header.paddingLength = 0;
+              rec->header.reserved = 0;
+
+              // The appStatus component is an application-level status code.
+              // Each role documents its usage of appStatus.
+              uint32_t appStatus = 0;
+              // The protocolStatus component is a protocol-level status code.
+              uint8_t  protocolStatus = FCGI_CANT_MPX_CONN;
+
+              rec->body.appStatusB3    = (uint8_t) ((appStatus >> 24) & 0xff);
+              rec->body.appStatusB2    = (uint8_t) ((appStatus >> 16) & 0xff);
+              rec->body.appStatusB1    = (uint8_t) ((appStatus >>  8) & 0xff);
+              rec->body.appStatusB0    = (uint8_t) ((appStatus      ) & 0xff);
+              rec->body.protocolStatus = (uint8_t) protocolStatus;
+              memset (rec->body.reserved, 0, sizeof (rec->body.reserved));
+              conn->state = SEND;
+              conn->pos = 0;
+              conn->packet_len = (sizeof (rec->body));
+              c_loop_remove_fd (loop, sockfd);
+              c_loop_add_fd (loop, sockfd, POLLOUT, cb_send, conn);
+              return;
+              */
+            }
+
+            break;
+          case FCGI_PARAMS: // 4
+            {
+              c_log_info ("FCGI_PARAMS");
+              conn->state = RECV_HEADER;
+              conn->pos = 0;
+
+              uint8_t name_len;
+              uint8_t value_len;
+              uint8_t* p = conn->buf;
+
+              while (p < conn->buf + conn->content_len)
+              {
+                name_len  = *p; p++;
+                value_len = *p; p++;
+                printf ("%.*s: %.*s\n",
+                        name_len, p,
+                        value_len, p + name_len);
+                p = p + name_len + value_len;
+              }
+            }
+
+            break;
+
+          case FCGI_ABORT_REQUEST: // 2
+          case FCGI_END_REQUEST: // 3
+          case FCGI_STDIN: // 5
+          case FCGI_STDOUT: // 6
+          case FCGI_STDERR: // 7
+          case FCGI_DATA: // 8
+          case FCGI_GET_VALUES: // 9
+          case FCGI_GET_VALUES_RESULT: // 10
+          case FCGI_UNKNOWN_TYPE: // 11
+          default:
+            c_log_warning ("programming error: type id: %hu", conn->type);
+            abort ();
+        }
+      }
     }
   }
   else if (n_bytes == 0)
@@ -257,6 +315,7 @@ static void cb_read_header (int sockfd, short revents, void* user_data)
     if (errno == EWOULDBLOCK ||
         errno == EAGAIN)
     {
+      // retry
       c_log_info ("%s", strerror (errno));
     }
     else
@@ -293,7 +352,7 @@ static void cb_new_req (int sockfd, short revents, void* user_data)
   }
 
   Conn* conn = conn_new ();
-  c_loop_add_fd (loop, client_fd, POLLIN, cb_read_header, conn);
+  c_loop_add_fd (loop, client_fd, POLLIN, cb_incoming, conn);
 }
 
 static void cb_quit (int signo)
